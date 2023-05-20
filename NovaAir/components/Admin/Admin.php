3535e323de2d1aa7bea9888a37d09523976ad902
SAwandya <sachilaawandya@gmail.com>
2023-05-20 23:15:51 +0530

    dashboard_2.0
    create dashboard with css without connecting to database

---
+++ NovaAir/components/Admin/Admin.php
@@ -0,0 +1,67 @@
+<div class="board">
+    <h3>Manage Flights</h3>
+    <hr>
+    <table width="100%">
+        <thead>
+            <tr>
+                <td>Flight ID</td>
+                <td>Route</td>
+                <td>Delete</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="people">
+                    <img src="./dashboard_profile_pic/WhatsApp Image 2023-05-14 at 00.01.52.jpg" alt="">
+                    <div class="people-de">
+                        <h5>SA5518</h5>
+                        
+                    </div>
+                </td>
+
+                <td class="people-des">
+                    <h5>Colombo to Dubai</h5>
+                </td>
+
+                <td class="role">
+                    <p><button>Delete</button></p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+
+
+<div class="board">
+    <h3>New Feedbacks</h3>
+    <hr>
+    <table width="100%">
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Feedback</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="people">
+                    <div class="people-de">
+                        <h5>John Doe</h5>
+                        
+                    </div>
+                </td>
+
+                <td class="people-des">
+                    <h5>sachilaawandya@gmail.com</h5>
+                    
+                </td>
+
+                <td>Lorem ipsum is typically a corrupted version of De finibus bonorum et malorum, a 
+                    1st-century BC text by the Roman statesman and philosopher Cicero, with words altered, 
+                    added, and removed to make it nonsensical and improper Latin. The first two words themselves
+                     are a truncation of 'dolorem ipsum'</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
---
+++ NovaAir/components/System_Admin/System_admin.php
@@ -0,0 +1,65 @@
+<div class="values">
+                
+    <div class="val-box">
+        <i class="fas fa-users"></i>
+        <div>
+            <h3>17</h3>
+            <span>Total Flights</span>
+        </div>
+    </div>
+    <div class="val-box">
+        <i class="fas fa-users"></i>
+        <div>
+            <h3>9</h3>
+            <span>Total Planes Grounded</span>
+        </div>
+    </div>
+    <div class="val-box">
+        <i class="fas fa-users"></i>
+        <div>
+            <h3>8</h3>
+            <span>Total planes in Flights</span>
+        </div>
+    </div>
+    
+</div>
+
+<div class="board">
+    <h3>Manage Admins</h3>
+    <hr>
+    <table width="100%">
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Plane ID</td>
+                <td>Status</td>
+                <td>Delete</td>
+                <td>Edit</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="people">
+                    <img src="./dashboard_profile_pic/WhatsApp Image 2023-05-14 at 00.01.52.jpg" alt="">
+                    <div class="people-de">
+                        <h5>John Doe</h5>
+                        <p>john@example.com</p>
+                    </div>
+                </td>
+
+                <td class="people-des">
+                    <h5>A104</h5>
+                    <p>NovaAir</p>
+                </td>
+
+                <td class="active"><p>Active</p></td>
+
+                <td class="role">
+                    <p><button>Delete</button></p>
+                </td>
+
+                <td class="edit"><a href="#">edit</a></td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
